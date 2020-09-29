e292481758e5a29eef9ca43ebd2c6cbb4c5ae65e
Ivar Lundin <60367191+ivarlundin@users.noreply.github.com>
2020-09-29 14:50:00 +0200

    new version

--- v2/pointlights/pointlights.ino
+++ v2/pointlights/pointlights.ino
@@ -18,9 +18,23 @@ int chargeState;    //255 is 100 percent
 int holdChargeState;    //255 is 100 percent
 int pauseTime;    //255 is 100 percent
 int storeState;
-
+int fading = 0; 
 //State
 int globalState = 0;
+int painCounter = 0; 
+int painSwitch = 0; 
+
+//Am I alive?
+int alive = 1;
+
+int hlrCount = 0;
+int hlrFreqCheck = 0;
+int lifeCounter = 0;
+int requiredHlrCount = 10;
+int sensitivity = 0;
+
+int lives = 2;
+int maxPain = 20;
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,73 +43,123 @@ void setup() {
 
   //Sensor
   pinMode(A0, INPUT);
+
+  //Start over
+  pinMode(10, INPUT_PULLUP);
 }
 
 void loop() {
-  //Read sensor value
-  int sensorValue = analogRead(A0);
-  sensorValue = sensorValue * 10;
-  sensorValue = map(sensorValue, 0, 9500, 0, 100);
-  
-  if (sensorValue > 100) {    //Set max value
-    sensorValue = 100;
-  }   
-  //Serial.print("Sensor : ");
-  //Serial.println(sensorValue);
-  
-  //brightness = map(sensorValue, 0, 100, 0, 255);
-  
-  //Map sensor value to conditions and state  
-  if (sensorValue > 80) {     //100
-    globalState = 1;
-    //Serial.println("above 80");
 
-  } else if (sensorValue > 50) {    //75
-    globalState = 2;
-    //Serial.println("above 50");
+  if (alive == 0) {
+    if (hlrCount >= requiredHlrCount && lifeCounter < lives) {
+      Serial.println("Alive!!!!");
+      alive = 1;
+      lifeCounter++;
+      requiredHlrCount = requiredHlrCount + 5;
 
-  } else if (sensorValue > 20) {            //0
-    globalState = 3;
-    //Serial.println("else");
+      //reset those badboys
+      hlrCount = 0;
+      hlrFreqCheck = 0;
+
+      //painswitch?
+      
+    } else {
+      die();
+    
+      int startPin = digitalRead(10);
+      
+      if (startPin == 0 && hlrFreqCheck == 0) {
+        Serial.println("Wake up!");
+        hlrCount++;
+        hlrFreqCheck = 1;
+
+        
+      } else if (startPin == 1) {
+        hlrFreqCheck = 0;
+      } 
+      delay(10);
+    }
+       
   } else {
-    globalState = 4;
-  }
-
-  //States
-  if (globalState == 1) {        // 100
-   //Define variables
-    increaseDecrease();      //Composing function
-
-    delay(10);
-    analogWrite(ledPin, brightness);
-    currentMillis = millis(); //store the current time since the program started
-
-   } else if (globalState == 2) {        // 75
-    //Define variables
-    maxLength = 100;
-    chargeState = 130;    //255 is 100 percent
-    holdChargeState = 1200;    //255 is 100 percent
-    pauseTime = 1000;    //255 is 100 percent
+    //Read sensor value
+    int sensorValue = analogRead(A0);
+    sensorValue = sensorValue * 10;
+    sensorValue = map(sensorValue, 0, 9500, 0, 100);
     
-    changeState(WAVE);
-    waveThem();    //Composing function
-
-    delay(10);
-    analogWrite(ledPin, brightness);
-    currentMillis = millis(); //store the current time since the program started
+    if (sensorValue > 100) {    //Set max value
+      sensorValue = 100;
+    }   
+    //Serial.print("Sensor : ");
+    //Serial.println(sensorValue);
     
-  } else if (globalState == 3) {        // 50
-    waveSlow();
-    delay(10);
-    analogWrite(ledPin, brightness);
-    currentMillis = millis(); //store the current time since the program started
+    //brightness = map(sensorValue, 0, 100, 0, 255);
+    
+    //Map sensor value to conditions and state  
+    if (sensorValue > 95) {     //100
+      globalState = 1;
+      //Serial.println("above 80");
+  
+    } else if (sensorValue > 50 - sensitivity) {    //75
+      globalState = 2;
+      //Serial.println("above 50");
+  
+    } else if (sensorValue > 5 - sensitivity) {            //0
+      globalState = 3;
+      //Serial.println("else");
+    } else {
+      globalState = 4;
+    }
 
-  } else if (globalState == 4) {
-    constantWave();
-    delay(10);
-    analogWrite(ledPin, brightness);
-    currentMillis = millis(); //store the current time since the program started
-
+    //States
+    if (globalState == 1) {        // 100
+     //Define variables
+       if (painCounter > maxPain) {
+        die(); 
+       } else {
+        increaseDecrease();      //Composing function
+       }
+      delay(10);
+      analogWrite(ledPin, brightness);
+      currentMillis = millis(); //store the current time since the program started
+  
+     } else if (globalState == 2) {        // 75
+      //Define variables
+      maxLength = 100;
+      chargeState = 130;    //255 is 100 percent
+      holdChargeState = 1200;    //255 is 100 percent
+      pauseTime = 1000;    //255 is 100 percent
+      
+      changeState(WAVE);
+      waveThem();    //Composing function
+  
+      delay(10);
+      analogWrite(ledPin, brightness);
+      currentMillis = millis(); //store the current time since the program started
+      
+    } else if (globalState == 3) {        // 50
+      waveSlow();
+      delay(10);
+      analogWrite(ledPin, brightness);
+      currentMillis = millis(); //store the current time since the program started
+  
+    } else if (globalState == 4) {
+    
+      if (painCounter % 2 == true && painSwitch == 0) {
+        painSwitch = 1;
+      } else {
+        painCounter = painCounter * 0.99999997;
+        painSwitch = 0;
+      }
+      
+      Serial.print("The current pain is: ");
+      Serial.print(painCounter);
+      Serial.println(" painThymes");
+      constantWave();
+      delay(10);
+      analogWrite(ledPin, brightness);
+      currentMillis = millis(); //store the current time since the program started
+  
+    }
   }
 }
 
@@ -196,8 +260,8 @@ void compose() {
 
 void waveThem() {
   plot("WAVE", brightness);
-  brightness = 180;
-  brightness = sinewave(1000,256,0); // you can tweak the parameters of the sinewave
+  brightness = brightness;
+  brightness = sinewave(1000,180,50); // you can tweak the parameters of the sinewave
   analogWrite(ledPin, brightness);
   
   if (currentMillis - startMillis >= 5000){ //change state after 5 secs by comparing the time elapsed since we last change state
@@ -206,10 +270,11 @@ void waveThem() {
 }
 void waveSlow() {
   plot("WAVE", brightness);
-  brightness = 180;
-  brightness = sinewave(2000,256,0); // you can tweak the parameters of the sinewave
-  analogWrite(ledPin, brightness);
+  brightness = brightness;
+  brightness = sinewave(2000,100, 25); // you can tweak the parameters of the sinewave
   
+  analogWrite(ledPin, brightness);
+
   if (currentMillis - startMillis >= 5000){ //change state after 5 secs by comparing the time elapsed since we last change state
     changeState(START);
     }
@@ -217,22 +282,21 @@ void waveSlow() {
 
 void constantWave() {
   plot("WAVE", brightness);
-  brightness = 180;
-  brightness = sinewave(3000,80,0); // you can tweak the parameters of the sinewave
-  analogWrite(ledPin, brightness);
+  brightness = brightness;
+  brightness = sinewave(3000,50,0); // you can tweak the parameters of the sinewave
   
   if (currentMillis - startMillis >= 5000){ //change state after 5 secs by comparing the time elapsed since we last change state
     changeState(START);
     }
 }
 void increaseDecrease() {
-  if (ledState == 0) {
+  if (ledState == 0 || ledState == 4) {
     changeState(INCREASE);
   }
   switch (ledState){
 
   case INCREASE:
-    brightness = increase_brightness(brightness, 40);
+    brightness = increase_brightness(brightness, 20 + (painCounter * painCounter/2));
     //brightness = expInc_brightness(brightness, 20);
     
     plot("INCREASING", brightness);
@@ -244,18 +308,34 @@ void increaseDecrease() {
    break;
    
   case DECREASE:
-    brightness = decrease_brightness(brightness, 70);
+    brightness = decrease_brightness(brightness, 40 + (painCounter * painCounter/2));
     //brightness = expDec_brightness(brightness, maxLength/10);
     
     plot("DECREASING", brightness);
       if (brightness <= 0){
       changeState(INCREASE);
+      painCounter++;
+      Serial.print("Pain for: ");
+      Serial.print(painCounter);
+      Serial.println(" painThymes");
       }
      break;
   }
 }
 
 //Helper functions
+void die() {
+  alive = 0;
+     
+  if (brightness <= 0){
+    //Callback for died
+    //Serial.print("died ... ");
+  } else {
+    brightness = expDec_brightness(brightness, 2);
+  
+    plot("DECREASING", brightness);
+  }
+}
 
 void changeState(ledStates newState){
     // call to change state, will keep track of time since last state
@@ -312,6 +392,7 @@ int sinewave(float duration, float amplitude, int offset){
     float period = millis()/duration; // Duration in ms determines the wavelength.
     float midpoint = amplitude / 2; // set the midpoint of the wave at half the amplitude so there are no negative numbers
     int value = midpoint + midpoint * sin ( period * 2.0 * PI );
+    
     value = value + offset; //offset allows you to move the wave up and down on the Y-axis. Should not exceed the value of amplitude to prevent clipping.
     return value;
   }
